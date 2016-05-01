cb309a1df1be7d860ddd841bf59c4c1613789e94
suleymanarslan <me@suleymanarslan.com>
2016-05-02 01:08:19 +0300

    model update

--- services/models/interviewer.go
+++ services/models/interviewer.go
@@ -1,5 +1,12 @@
 package models
 
+import(
+    "time"
+)
+
 type Interviewer struct{
-    
+    Id string `json:"uuid" form:"-"`
+    UserId string `json:"uuid" form:"user"`
+    Ranking int `json:"uuid" form:"ranking"`
+    DateJoined time.Time `json:"uuid" form:"joined"`
 }
